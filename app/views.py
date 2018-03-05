d55416756029d3550e70631186623ce5fe201119
Michael Calloo <michael.calloo@mymona.uwi.edu>
2018-03-05 04:53:15 +0000

    problem final

--- app/views.py
+++ app/views.py
@@ -60,6 +60,11 @@ def login():
 def secure_page():
     return render_template("secure_page.html")
     
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash("You have successfully logged out")
+    return redirect(url_for("home"))
     
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
